07cb0f586f97f86dc59957660ecad819c556052b
Ajay Shankar R <shankarajay33@gmail.com>
2025-02-10 01:29:32 +0530

    added extra pages

--- src/Header/NavLinks.tsx
+++ src/Header/NavLinks.tsx
@@ -10,8 +10,10 @@ const NavLinks = () => {
 
   const location = useLocation();
   return (
-    <div className="flex gap-5 h-full items-center">
-      {links.map((link, index) => <div className={'border-t-[3px] h-full'}>
+    <div className="flex gap-5 text-mine-shaft-300 h-full items-center">
+      {links.map((link, index) => <div className={`${location.pathname === (link.url.startsWith('/') ? link.url : '/' + link.url) ? 
+        "border-bright-sun-400 text-bright-sun-400" : 
+        "border-transparent"} border-t-[3px] h-full flex items-center`}>
         <Link key={index} to={link.url}>
           {link.name}
         </Link>
--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,9 +1,11 @@
 import Header from "../Header/Header";
+import DreamJob from "../LandingPage/DreamJob";
 
 const Homepage = () => {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
       <Header />
+      <DreamJob/>
     </div>
   )
 }
