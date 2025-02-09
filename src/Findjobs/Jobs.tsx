d3d145873209b60909a6fc5bc369111ce1c141c1
Saran-067 <saranrajasekaran067@gmail.com>
2025-02-09 18:38:30 +0530

    small change

---
+++ src/Findjobs/Jobs.tsx
@@ -0,0 +1,10 @@
+const Jobs = () => {
+    return (
+        <div>
+            <div className="Recommended jobs">
+                
+            </div>
+            <h1>Jobs</h1>
+        </div>
+    )
+}
